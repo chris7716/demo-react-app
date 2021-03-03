a73237aed2c05368d32d3d8baea42ee31d4dd921
Hasitha Kaushan <hkraclh@gmail.com>
2021-03-03 16:13:48 +0530

    home moved to containers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Home } from './components';
+import { Home } from './containers';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
